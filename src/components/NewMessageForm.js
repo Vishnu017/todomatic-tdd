644e91b4a1743f29fc364663db331b887fb9dec5
Vishnu017 <vishnuprasadkm09@gmail.com>
2021-09-06 15:25:29 +0530

    Added Cypress

---
+++ src/components/NewMessageForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NewMessageForm = () => {
+  return (
+    <div>
+
+    <input
+        type="text"
+        data-testid="messageText"
+     />
+     <button
+        data-testid="sendButton"
+      >
+       Send
+      </button>
+    </div>
+  );
+};
+
+export default NewMessageForm;
